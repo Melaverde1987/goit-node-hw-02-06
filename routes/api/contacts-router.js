a0a8e616787dad5cb51dbd21a7352b0ca41e0fc1
Melaverde1987 <79062250+Melaverde1987@users.noreply.github.com>
2024-01-04 23:33:24 +0100

    error correction

--- routes/api/contacts-router.js
+++ routes/api/contacts-router.js
@@ -2,22 +2,32 @@ import express from "express";
 
 import contactsController from "../../controllers/contacts-controller.js";
 
-import { isEmptyBody, isEmptyBodyFavorite } from "../../middlewares/index.js";
+import {
+  isEmptyBody,
+  isEmptyBodyFavorite,
+  isValidId,
+} from "../../middlewares/index.js";
 
 const contactsRouter = express.Router();
 
 contactsRouter.get("/", contactsController.getAll);
 
-contactsRouter.get("/:id", contactsController.getById);
+contactsRouter.get("/:id", isValidId, contactsController.getById);
 
 contactsRouter.post("/", isEmptyBody, contactsController.add);
 
-contactsRouter.put("/:id", isEmptyBody, contactsController.updateById);
+contactsRouter.put(
+  "/:id",
+  isValidId,
+  isEmptyBody,
+  contactsController.updateById
+);
 
-contactsRouter.delete("/:id", contactsController.deleteById);
+contactsRouter.delete("/:id", isValidId, contactsController.deleteById);
 
 contactsRouter.patch(
   "/:id/favorite",
+  isValidId,
   isEmptyBodyFavorite,
   contactsController.updateStatusContact
 );
